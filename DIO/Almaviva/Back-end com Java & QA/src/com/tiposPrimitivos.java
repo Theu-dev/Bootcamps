b46859155494a930fa563c321189665e84a2fdda
Matheus R Ribeiro <matheusdev060802@gmail.com>
2026-03-27 22:08:34 -0300

    feat: adiciona script de entrada e saida de nome e idade no arquivo tiposPrimitivos

--- DIO/Almaviva/Back-end com Java & QA/src/com/tiposPrimitivos.java
+++ DIO/Almaviva/Back-end com Java & QA/src/com/tiposPrimitivos.java
@@ -4,4 +4,14 @@ import java.util.Scanner;
 
 public class tiposPrimitivos {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Olá, informe o seu nome: ");
+        String name = scanner.next();
+
+        System.out.println("Informe sua idade: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Olá " + name + " Sua idade é " + age);
+    }
 }
